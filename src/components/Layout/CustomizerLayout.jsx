094f77c1ecc1176ae3bf3a60795af8ea99c45c24
Bercerq <nice123414700741@gmail.com>
2022-02-16 17:58:40 +0200

    merge fix

--- src/components/Layout/CustomizerLayout.jsx
+++ src/components/Layout/CustomizerLayout.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { MainWrapper } from "../Sidebar/style";
 import TextEditor from "../TextEditor/TextEditor";
-import SideBar from "../Sidebar/Sidebar";
 import Confetti from "../Confetti/Confetti";
+import Sidebar from "../Sidebar/Sidebar";
 
 function CustomizerLayout({ title, children, сonfettiData }) {
   return (
     <MainWrapper>
       <title>{title}</title>
-      <SideBar />
+      <Sidebar />
       {children}
       <Confetti сonfettiData={сonfettiData} />
       <TextEditor />
