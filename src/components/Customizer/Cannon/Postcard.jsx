0a81f74f7954fec0674bc652be8c3a471838a36f
Kirill Nefedov <kirill.nefodov@devit.group>
2022-04-05 13:32:10 +0300

    deleted modal window

--- src/components/Customizer/Cannon/Postcard.jsx
+++ src/components/Customizer/Cannon/Postcard.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 
 import PostcardSection from "./PostcardSection/PostcardSection";
-import BlueButton from "../../Buttons/BlueButton";
-import Modal from "../../Modal/Modal";
 
 import {PostcardBlock} from "./PostcardSection/style";
 
 const Postcard = ({editTextRef}) => {
   return (
-    <>
-      <PostcardBlock>
-        <PostcardSection editTextRef={editTextRef}/>
-      </PostcardBlock>
-      <Modal>
-        <h1 style={{textAling: "center"}}>
-          Item add
-        </h1>
-        <BlueButton>Checkout</BlueButton>{" "}
-      </Modal>
-    </>
+    <PostcardBlock>
+      <PostcardSection editTextRef={editTextRef}/>
+    </PostcardBlock>
   );
 };
 
